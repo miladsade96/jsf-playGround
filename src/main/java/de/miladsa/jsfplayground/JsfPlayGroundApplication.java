c45b387be3b12de547d8df0d193799dced750e99
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-01 11:59:08 +0330

    Initial commit

---
+++ src/main/java/de/miladsa/jsfplayground/JsfPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package de.miladsa.jsfplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfPlayGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsfPlayGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/miladsa/jsfplayground/JsfPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package de.miladsa.jsfplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsfPlayGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
